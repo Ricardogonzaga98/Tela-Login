ca823681ca3f332e24ce25431e01a4da9c6ee6ba
Ricardinho <ricardogonzaga98@gmail.com>
2025-04-15 19:52:47 -0300

    Login email e Login senha

---
+++ main.js
@@ -0,0 +1,13 @@
+// Implementa acesso para a tela de login
+function acessar() {
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+
+    // implementa estrutura que testa se todos os campos foram preenchidos
+    if(!loginEmail || !loginSenha){
+        alert("Favor preencher todos os campos");
+    } else {
+        window.location.href = "cadastro.html";
+    }
+
+}
