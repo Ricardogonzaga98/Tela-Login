e4de704877126fc2936b15c8d4b624a2a0356eff
Ricardinho <ricardogonzaga98@gmail.com>
2025-04-15 21:55:29 -0300

    funcionalidade de editar e excluir

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ function salvarUser(){
     if(nomeUser){
         dadosLista.push(nomeUser);
         console.log(dadosLista);
+        criaLista();
         document.getElementById ('nomeUser').value ="";
     }else {
         alert("Favor informar um nome para cadastro");
@@ -29,10 +30,25 @@ function salvarUser(){
 // Função para preencher a lista de cadastro
 
 function criaLista(){
-    let tabela = "<tr> <th>Nome</th><th>Ações</th></tr>";
+    let tabela = "<tr> <th>Nome</th><th></th></tr>";
     for(let i = 0; i <= (dadosLista.length -1); i ++){
-        tabela += "<tr><tr>" + dadosLista [i] + "</th><th>Ações</th></tr>";
+        tabela += "<tr><td>" + dadosLista [i] + "</td><td> <button class='btn btn-warning' onclick=''>Editar <button class='btn btn-danger' onclick= 'excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button><td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
 
     }
+}
+
+//Função para excluir o nome da lista
+
+function excluir(i){
+    dadosLista.splice((i-1), 1);
+    document.getElementById('tabela').deleteRow(i);
+    
+}
+
+// Função para edição de nome da lista
+function editar(i){
+    document.getElementById('nomeUser').
+    value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)],1);
 }
