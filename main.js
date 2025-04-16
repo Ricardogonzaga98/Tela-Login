2f99b0f79babacfc77b7d6e2762c27d8caee4917
Ricardinho <ricardogonzaga98@gmail.com>
2025-04-15 22:01:00 -0300

    Cadastro Atualizado de editação e excluir

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function salvarUser(){
 function criaLista(){
     let tabela = "<tr> <th>Nome</th><th></th></tr>";
     for(let i = 0; i <= (dadosLista.length -1); i ++){
-        tabela += "<tr><td>" + dadosLista [i] + "</td><td> <button class='btn btn-warning' onclick=''>Editar <button class='btn btn-danger' onclick= 'excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button><td></tr>";
+        tabela += "<tr><td>" + dadosLista [i] + "</td><td> <button class='btn btn-warning' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar <button class='btn btn-danger' onclick= 'excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button><td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
 
     }
@@ -50,5 +50,6 @@ function excluir(i){
 function editar(i){
     document.getElementById('nomeUser').
     value = dadosLista[(i - 1)];
-    dadosLista.splice(dadosLista[(i - 1)],1);
+    dadosLista.splice((i - 1),1);
+    
 }
